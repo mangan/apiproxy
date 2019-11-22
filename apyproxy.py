1d75ebff899e12c53c65cc57e0900a6e61242e5d
mangan <mangan@localhost>
2019-11-22 13:56:44 +0100

    kickoff

---
+++ apyproxy.py
@@ -0,0 +1,103 @@
+import os.path
+import re
+import unittest
+
+from urllib.parse import urljoin, urlsplit
+
+import requests
+
+
+def bind(cls, relpath, name):
+    def decorator(fn):
+        if cls._ApyProxy__bindings is None:
+            cls._ApyProxy__bindings = {}
+        cls._ApyProxy__bindings[name] = (relpath, fn)
+        return fn
+    return decorator
+
+
+def context(proxy, key):
+    return proxy._ApyProxy__context.group(key)
+
+
+class UnboundCallError(Exception):
+    pass
+
+
+class _Pattern:
+    def __init__(self, pattern):
+        self.pattern = pattern
+
+    def _tr(self):
+        return "%s$" % re.sub('\{([^/]*)\}', r'(?P<\1>[^/]*)', self.pattern)
+
+    def match(self, relpath):
+        return re.match(self._tr(), relpath)
+
+
+class ApyProxy:
+    __bindings = None
+
+    def __init__(self, url, session=None):
+        self.__url = url
+        self.__session = session or requests.Session()
+        self.__parent = None
+
+    def __enter__(self):
+        self.__session.__enter__()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.__session.__exit__(exc_type, exc_val, exc_tb)
+
+    def __call__(self, *args, **kwargs):
+        call = self.__url.rstrip("/").split("/")[-1]
+        here = urlsplit(self.__parent._url).path
+        if call in self.__bindings:
+            pattern, fn = self.__bindings[call]
+            match = _Pattern(pattern).match(here)
+            if match:
+                self.__context = match
+                return fn(self, *args, **kwargs)
+        raise UnboundCallError(f"'{call}' is not bound to '{here}'")
+
+    def __getattr__(self, name):
+        return self._(name)
+
+    def _(self, relpath):
+        relpath = str(relpath)
+        if relpath.startswith("/"):
+            url = urljoin(self.__url, relpath)
+        else:
+            url = os.path.join(self.__url.rstrip("/"), relpath)
+        proxy = ApyProxy(url, self.__session)
+        proxy._ApyProxy__parent = self
+        return proxy
+
+    @property
+    def _url(self):
+        url = self.__url
+        if hasattr(self.__session, "suffix") and not url.endswith("/"):
+            url = f"{url}{self.__session.suffix}"
+        return url
+
+    def request(self, method, **kwargs):
+        return self.__session.request(method, self._url, **kwargs)
+
+    def get(self, params=None, **kwargs):
+        return self.request("GET", params=params, **kwargs)
+
+    def head(self, **kwargs):
+        return self.request("HEAD", **kwargs)
+
+    def patch(self, data=None, **kwargs):
+        return self.request("PATCH", data=data, **kwargs)
+
+    def post(self, data=None, json=None, **kwargs):
+        return self.request("POST", data=data, json=json, **kwargs)
+
+    def put(self, data=None, **kwargs):
+        return self.request("PUT", data=data, **kwargs)
+
+    def __repr__(self):
+        return "ApyProxy(%s)" % self._url
---
+++ test_apyproxy.py
@@ -0,0 +1,97 @@
+from apyproxy import *
+from apyproxy import _Pattern
+
+
+class _PatternTest(unittest.TestCase):
+    def test_tr(self):
+        pattern = "/this/{pattern}/that/{subs}/do"
+        expected = "/this/(?P<pattern>[^/]*)/that/(?P<subs>[^/]*)/do$"
+        translated = _Pattern(pattern)._tr()
+
+        self.assertEqual(translated, expected)
+
+    def test_match(self):
+        pattern = _Pattern("/this/{pattern}/that")
+
+        self.assertTrue(pattern.match("/this/{pattern}/that"))
+        self.assertTrue(pattern.match("/this/is/that"))
+        self.assertFalse(pattern.match("/this/is/not/that"))
+        self.assertFalse(pattern.match("/other/is/that"))
+        self.assertFalse(pattern.match("/other/is/that/"))
+        self.assertFalse(pattern.match("/top/this/is/that"))
+        self.assertFalse(pattern.match("/this/is/that/tail"))
+        self.assertEqual(pattern.match("/this/is/that").group("pattern"), "is")
+
+
+class ApyProxyTest(unittest.TestCase):
+    def test_ApyProxy(self):
+        baseurl = "https://httpbin.org"
+        url = baseurl
+        with ApyProxy(url) as r:
+            self.assertEqual(repr(r), "ApyProxy(%s)" % url)
+
+            url = "%s/anything" % url
+            r = r.anything
+            self.assertEqual(repr(r), "ApyProxy(%s)" % url)
+
+            url = "%s/this" % url
+            r = r.this
+            self.assertEqual(repr(r), "ApyProxy(%s)" % url)
+
+            url = "%s/get" % baseurl
+            r = r._("/get")
+            self.assertEqual(repr(r), "ApyProxy(%s)" % url)
+
+            url = "%s/anything/else" % baseurl
+            r = r._("/anything/else")
+            self.assertEqual(repr(r), "ApyProxy(%s)" % url)
+
+            url = "%s/than/this" % url
+            r = r.than.this
+            self.assertEqual(repr(r), "ApyProxy(%s)" % url)
+
+        with ApyProxy(baseurl) as r:
+            response = r._("get").get()
+            self.assertEqual(response.status_code, 200)
+            claimed = response.json()
+            self.assertEqual(claimed["url"], "%s/get" % baseurl)
+
+            params = {"key": "value"}
+            response = r._("get").get(params)
+            self.assertEqual(response.status_code, 200)
+            claimed = response.json()
+            self.assertEqual(claimed["args"], params)
+
+        session = requests.Session()
+        session.suffix = ".xml"
+        with ApyProxy(baseurl, session) as r:
+            response = r.anything.object.get()
+
+            self.assertEqual(response.status_code, 200)
+            claimed = response.json()
+            self.assertEqual(claimed["url"], "%s/anything/object.xml" % baseurl)
+
+        session = requests.Session()
+        session.params = params
+        with ApyProxy(baseurl, session) as r:
+            response = r.anything.reasonable.get()
+            self.assertEqual(response.status_code, 200)
+            claimed = response.json()
+            self.assertEqual(claimed["args"], params)
+
+            params2 = params.copy()
+            params2.update({"this": "that"})
+            response = r.anything.unreasonable.get({"this": "that"})
+            self.assertEqual(response.status_code, 200)
+            claimed = response.json()
+            self.assertEqual(claimed["args"], params2)
+
+    def test_bind(self):
+        baseurl = "https://httpbin.org"
+        api = ApyProxy(baseurl)
+
+        @bind(ApyProxy, "/anything/{name}/chat", "say_hello")
+        def say_hello(chat, msg):
+            return "Hello %s, %s" % (context(chat, "name"), msg)
+
+        self.assertEqual(api.anything.jack.chat.say_hello("kuk"), "Hello jack, kuk")
